ef93c635d9f55698731ea25e3b684c81868a9424
sushmita <mmsushma9999@gmail.com>
2020-10-23 12:51:38 +0530

    css loaders

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import {other} from './other';
+import './style.css';
 var name = require('./name');
 alert(name);
 document.write(other());
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,20 @@ module.exports = {
     entry:"./src/app.js",
     output:{
         path:path.resolve(__dirname,'dist'),
-        filename:"bundle.js"
+        filename:"output.js"
+    },
+    module:{
+        rules:[{
+            test:/\.css$/,
+            use:[
+                'style-loader',
+                'css-loader'
+            ]
+        }]
+    },
+    devServer:{
+        contentBase:path.join(__dirname,'dist'),
+        compress:true,
+        port:3800
     }
 }
